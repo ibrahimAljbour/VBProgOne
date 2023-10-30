c06441ab085022cece8328db1f6d1c0bbf341bc1
ibrahimAljbour <ibrahim.m.aljbour@gmail.com>
2023-10-30 19:52:46 +0300

    Add project files.

---
+++ VBProgOne/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VBProgOne
+{
+    public partial class FRM_LOGIN : Form
+    {
+        public FRM_LOGIN()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Welcome you in VB");
+        }
+    }
+}
